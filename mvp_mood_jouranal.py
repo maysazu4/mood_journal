5ecfe487ee7240c2c1d2b573124a51e001a03013
Marwan Taha <85227547+MarwanTahaCS@users.noreply.github.com>
2024-02-21 12:47:16 +0200

    adding user db

--- mvp_mood_jouranal.py
+++ mvp_mood_jouranal.py
@@ -1,10 +1,12 @@
+# importing json library
+from tinydb import TinyDB, Query
 
 def take_input():
     name = input("Please enter your name: ")
     get_input = input("Tell about your day: ")
     return get_input,name
 
-print(take_input())
+# print(take_input())
 
 moods = {
     "Happy": {"Happy", "Excited", "Delighted"},
@@ -12,3 +14,15 @@ moods = {
     "Anger": {"Furious", "Annoyed", "Irritated"},
 }
 
+def mood_journal(note):
+    with open('notes.txt', 'a') as file:
+        file.write(str(note) + '\n')
+
+# Create users Db
+users_db = TinyDB('users_db.json')
+
+# write users to db
+def add_user_json(user):
+    users_db.insert(user)
+
+# Note = Query()
