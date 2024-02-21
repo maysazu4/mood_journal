695a4c8b889dab2bcf9b411cf892255db6a2c4fa
hasanMasalha <106591427+hasanMasalha@users.noreply.github.com>
2024-02-21 14:14:55 +0200

    updated calculations

--- emotions_in_percent.py
+++ emotions_in_percent.py
@@ -1,5 +1,10 @@
 
-emotions_words={
+
+def emotion(sentences):
+        emotions_counter={}
+        emotions_sum = 0    
+        sent_splited = []
+        emotions_words={    
 "Joy":["Happy","Excited","Delighted","Blissful","Jubilant"],
 "Sadness":["Unhappy","Depressed","Sorrowful","Mournful","Melancholic"],
 "Anger":["Furious","Irritated","Enraged","Annoyed","Agitated"],
@@ -11,30 +16,33 @@ emotions_words={
 "Contentment":["Satisfied","Pleased","Gratified","Content","Fulfilled"],
 "Embarrassment":["Ashamed","Humiliated","Blushing","Self-conscious","Mortified"],
 }
-for key, value in emotions_words.items():
-    emotions_words[key] = [word.lower() for word in value]
+    
+        for key, value in emotions_words.items():
+            emotions_words[key] = [word.lower() for word in value]
+        for sent in sentences:
 
-def emotion(sent,emotions):
-    emotions_counter={}
-    emotions_sum = 0    
-    sent_splited = []
-    sent_splited= sent.split(" ")
-    for word in sent_splited:
-        for emotion in emotions.keys():
-            if word.lower() in emotions[emotion]:
+  
+            sent_splited= sent.split(" ")
+            for word in sent_splited:
+                for emotion in emotions_words.keys():
+                    if word.lower() in emotions_words[emotion]:
                 
-                emotions_sum+=1
-                if emotion in emotions_counter:
-                    emotions_counter[emotion] =  emotions_counter[emotion]+1
-                else :
-                    emotions_counter[emotion] = 1
-    emotions_percents = {}
-    for emotion1 in emotions_counter.keys():
-        print(emotion1)
-        emotions_percents[emotion1]= emotions_counter[emotion1] /emotions_sum *100/100
-        print(emotion1 + " : " + str(emotions_percents[emotion1]))
-sent1 = "i feel happy and Unhappy in the same time"
-emotion(sent1,emotions_words)
+                        emotions_sum+=1
+                        if emotion in emotions_counter:
+                            emotions_counter[emotion] =  emotions_counter[emotion]+1
+                        else :
+                            emotions_counter[emotion] = 1
+
+        for emotion1 in emotions_counter.keys():
+             print(emotion1 + " : " + str(emotions_counter[emotion1]))
+
+    # emotions_percents = {}
+    # for emotion1 in emotions_counter.keys():
+    #     print(emotion1)
+    #     emotions_percents[emotion1]= emotions_counter[emotion1] /emotions_sum *100/100
+    #     print(emotion1 + " : " + str(emotions_percents[emotion1]))
+sent1 = ["i feel happy and Unhappy in the same time","kas askj Scared askl"]
+emotion(sent1)
 
 
 
