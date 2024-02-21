deda869d26499a90ac7c75817ef074cf8572da14
Marwan Taha <85227547+MarwanTahaCS@users.noreply.github.com>
2024-02-21 13:51:36 +0200

    adding id to db docs

--- mvp_mood_jouranal.py
+++ mvp_mood_jouranal.py
@@ -27,16 +27,20 @@ records_db = TinyDB('records_db.json')
 # write users to db
 def add_user(name):
     doc_id = users_db.insert({'name': name})
-    print(doc_id)
-add_user("mar")
+    users_db.update({'_id': doc_id}, doc_ids=[doc_id])
+add_user("name1")
 
 # write notes to db
 def add_note(user_id, note):
-    notes_db.insert({'user_id': user_id, 'note': note})
+    doc_id = notes_db.insert({'user_id': user_id, 'note': note})
+    notes_db.update({'_id': doc_id}, doc_ids=[doc_id])
+add_note(3, "I am happy")
 
 # write records to db
 def add_record(user_id, note_id, mood, date):
-    records_db.insert({'user_id': user_id, 'note_id': note_id, 'mood': mood, 'date': date})
+    doc_id = records_db.insert({'user_id': user_id, 'note_id': note_id, 'mood': mood, 'date': date})
+    records_db.update({'_id': doc_id}, doc_ids=[doc_id])
+add_record(3, 1, "Happy", time.time())
 
 # def save_note_details_to_DB(user_id):
 #     add_note(user_id, take_input())
